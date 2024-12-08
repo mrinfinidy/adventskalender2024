6bde0f39cf5e82c5558c475bf1c0ae057f0bdbba
afkdev8 <mr.infinidy@protonmail.com>
2024-12-08 22:59:01 +0100

    Replace day 6 and 8 with mp4

--- src/components/days.ts
+++ src/components/days.ts
@@ -2,168 +2,168 @@ import { ContentType } from "./utils"
 
 const day1 = {
   contentType: ContentType.Image,
-  contentSrc: "images/day1.jpg",
+  contentSrc: "content/day1.jpg",
   name: "Fenster 1",
   description: "Zum ersten Advent ein zuper siaßes winterliches Bild"
 }
 
 const day2 = {
   contentType: ContentType.Image,
-  contentSrc: "images/day2.jpg",
+  contentSrc: "content/day2.jpg",
   name: "Fenster 2",
   description: "Was hälts du von dem Café, das du mal an deinem Heimweg entdeckt hast?"
 }
 
 const day3 = {
   contentType: ContentType.Image,
-  contentSrc: "images/day3.jpg",
+  contentSrc: "content/day3.jpg",
   name: "Fenster 3",
   description: "Du bisch einfach siaß ♥ Oft sind es die Bilder, von einer normalen Situation, wo du einfach in die Kamera schaust wie du mich auch normal anschaust."
 }
 
 const day4 = {
   contentType: ContentType.Image,
-  contentSrc: "images/day4.png",
+  contentSrc: "content/day4.png",
   name: "Fenster 4",
   description: "Heute gibt es ein kleines italienisches Spielchen: https://puzzel.org/de/crossword/play?p=-ODBn2RNiLDneylmLpCb"
 }
 
 const day5 = {
   contentType: ContentType.Image,
-  contentSrc: "images/day5.png",
+  contentSrc: "content/day5.png",
   name: "Fenster 5",
   description: "Heute gibt es die Wiener Edition vom Kreuzworträtsel: https://www.xwords-generator.de/de/solve/tegqt"
 }
 
 const day6 = {
   contentType: ContentType.Image,
-  contentSrc: "images/day6.webp",
+  contentSrc: "content/day6.mp4",
   name: "Fenster 6",
   description: "Wie schön, dass du geboren bist...🎵 Ich freue mich jeden Tag, dass es dich gibt🐞"
 }
 
 const day7 = {
   contentType: ContentType.Image,
-  contentSrc: "images/day7.png",
+  contentSrc: "content/day7.png",
   name: "Fenster 7",
   description: "Heute gibt es die Karlsruher Edition vom Kreuzworträtsel: https://www.xwords-generator.de/de/solve/q3an4"
 }
 
 const day8 = {
   contentType: ContentType.Image,
-  contentSrc: "images/day8.webp",
+  contentSrc: "content/day8.mp4",
   name: "Fenster 8",
   description: "Das ist auch eines meiner Lieblingsvideos aus Italien. Es war dieser regnerische Tag, was aber auch ein besondere Atmosphäre hatte. Der Vormittag in diesem lokalen Café und dann die kurze Wanderung zur Piedigrotta - Wir zwei ganz allein."
 }
 
 const day9 = {
     contentType: ContentType.Video,
-  contentSrc: "images/day9.mp4",
+  contentSrc: "content/day9.mp4",
   name: "Fenster 9",
   description: "Hundi + Marlis 🐶 Ich hätte so gerne einen Hund mit dir ❤️"
 }
 
 const day10 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day11 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day12 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day13 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day14 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day15 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day16 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day17 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day18 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day19 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day20 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day21 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day22 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day23 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
 
 const day24 = {
   contentType: ContentType.Image,
-  contentSrc: "images/mojito.png",
+  contentSrc: "content/mojito.png",
   name: "Name Placeholder",
   description: "Description Placeholder"
 }
